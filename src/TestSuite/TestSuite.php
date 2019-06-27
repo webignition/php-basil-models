99ffde90929a6b759d34809b371cd5a4884cec99
Jon Cram <jon@webignition.net>
2019-06-27 15:29:16 +0100

    Add TestSuite::name

--- src/TestSuite/TestSuite.php
+++ src/TestSuite/TestSuite.php
@@ -7,20 +7,33 @@ use webignition\BasilModel\Test\TestInterface;
 class TestSuite implements TestSuiteInterface
 {
     /**
+     * @var string
+     */
+    private $name = '';
+
+    /**
      * @var TestInterface[]
      */
     private $tests = [];
 
     /**
+     * @param string $name
      * @param TestInterface[] $tests
      */
-    public function __construct(array $tests)
+    public function __construct(string $name, array $tests)
     {
+        $this->name = $name;
+
         foreach ($tests as $test) {
             if ($test instanceof TestInterface) {
                 $this->tests[] = $test;
             }
         }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
     }
 
     /**
