e54ac5120734d0efdeb9cc591948e20d80cd4844
Jon Cram <jon@webignition.net>
2019-07-16 12:14:53 +0100

    Create DataSetCollection

---
+++ tests/DataSet/DataSetCollectionTest.php
@@ -0,0 +1,144 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\BasilModel\Tests\DataSet;
+
+use webignition\BasilModel\DataSet\DataSet;
+use webignition\BasilModel\DataSet\DataSetCollection;
+
+class DataSetCollectionTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(array $dataSets, array $expectedDataSets)
+    {
+        $expectedCount = count($expectedDataSets);
+
+        $dataSetCollection = new DataSetCollection($dataSets);
+
+        $this->assertSame($expectedCount, count($dataSetCollection));
+
+        foreach ($dataSetCollection as $dataSetIndex => $dataSet) {
+            $expectedDataSet = $expectedDataSets[$dataSetIndex];
+
+            $this->assertEquals($expectedDataSet, $dataSet);
+        }
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'dataSets' => [],
+                'expectedDataSets' => [],
+            ],
+            'invalid items' => [
+                'dataSets' => [
+                    1,
+                    true,
+                    new \stdClass(),
+                ],
+                'expectedDataSets' => [],
+            ],
+            'valid items, numerical indices' => [
+                'dataSets' => [
+                    new DataSet([
+                        'username' => 'user1',
+                        'role' => 'user',
+                    ]),
+                    new DataSet([
+                        'username' => 'user2',
+                        'role' => 'admin',
+                    ]),
+                ],
+                'expectedDataSets' => [
+                    new DataSet([
+                        'username' => 'user1',
+                        'role' => 'user',
+                    ]),
+                    new DataSet([
+                        'username' => 'user2',
+                        'role' => 'admin',
+                    ]),
+                ],
+            ],
+            'valid items, string indices' => [
+                'dataSets' => [
+                    'set1' =>new DataSet([
+                        'username' => 'user1',
+                        'role' => 'user',
+                    ]),
+                    'set2' => new DataSet([
+                        'username' => 'user2',
+                        'role' => 'admin',
+                    ]),
+                ],
+                'expectedDataSets' => [
+                    'set1' =>new DataSet([
+                        'username' => 'user1',
+                        'role' => 'user',
+                    ]),
+                    'set2' => new DataSet([
+                        'username' => 'user2',
+                        'role' => 'admin',
+                    ]),
+                ],
+            ],
+        ];
+    }
+
+    public function testArrayAccess()
+    {
+        $set1 = new DataSet([
+            'username' => 'user1',
+            'role' => 'user',
+        ]);
+
+        $set2 = new DataSet([
+            'username' => 'user2',
+            'role' => 'admin',
+        ]);
+
+        $dataSetCollection = new DataSetCollection([]);
+
+        $this->assertEquals(new DataSetCollection(), $dataSetCollection);
+        $this->assertFalse($dataSetCollection->offsetExists(0));
+        $this->assertFalse($dataSetCollection->offsetExists(1));
+        $this->assertNull($dataSetCollection[0]);
+        $this->assertNull($dataSetCollection[1]);
+
+        $dataSetCollection[] = $set1;
+
+        $this->assertEquals(new DataSetCollection([$set1]), $dataSetCollection);
+        $this->assertTrue($dataSetCollection->offsetExists(0));
+        $this->assertFalse($dataSetCollection->offsetExists(1));
+        $this->assertSame($set1, $dataSetCollection[0]);
+        $this->assertNull($dataSetCollection[1]);
+
+        $dataSetCollection[] = $set2;
+
+        $this->assertEquals(new DataSetCollection([$set1, $set2,]), $dataSetCollection);
+
+        $this->assertTrue($dataSetCollection->offsetExists(0));
+        $this->assertTrue($dataSetCollection->offsetExists(1));
+        $this->assertSame($set1, $dataSetCollection[0]);
+        $this->assertSame($set2, $dataSetCollection[1]);
+
+        unset($dataSetCollection[0]);
+
+        $this->assertEquals(new DataSetCollection([1 => $set2,]), $dataSetCollection);
+        $this->assertFalse($dataSetCollection->offsetExists(0));
+        $this->assertTrue($dataSetCollection->offsetExists(1));
+        $this->assertNull($dataSetCollection[0]);
+        $this->assertSame($set2, $dataSetCollection[1]);
+
+        unset($dataSetCollection[1]);
+
+        $this->assertEquals(new DataSetCollection(), $dataSetCollection);
+        $this->assertFalse($dataSetCollection->offsetExists(0));
+        $this->assertFalse($dataSetCollection->offsetExists(1));
+        $this->assertNull($dataSetCollection[0]);
+        $this->assertNull($dataSetCollection[1]);
+    }
+}
