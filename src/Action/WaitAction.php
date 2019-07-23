2c7cfa06a1854e944d5a6c3416f0cf44e6b1b0d6
Jon Cram <webignition@gmail.com>
2019-07-23 10:41:48 +0100

    Wait action duration should be a value object (#79)

--- src/Action/WaitAction.php
+++ src/Action/WaitAction.php
@@ -2,18 +2,25 @@
 
 namespace webignition\BasilModel\Action;
 
+use webignition\BasilModel\Value\ValueInterface;
+
 class WaitAction extends AbstractAction implements WaitActionInterface
 {
     private $duration;
 
-    public function __construct(string $actionString, string $duration)
+    public function __construct(string $actionString, ValueInterface $duration)
     {
         parent::__construct($actionString, ActionTypes::WAIT, $duration, true);
 
         $this->duration = $duration;
     }
 
-    public function getDuration(): string
+    public function getArguments(): string
+    {
+        return trim(parent::getArguments(), '"');
+    }
+
+    public function getDuration(): ValueInterface
     {
         return $this->duration;
     }
--- tests/Action/WaitActionTest.php
+++ tests/Action/WaitActionTest.php
@@ -1,21 +1,52 @@
 <?php
+/** @noinspection PhpDocSignatureInspection */
 
 namespace webignition\BasilModel\Tests\Action;
 
 use webignition\BasilModel\Action\ActionTypes;
 use webignition\BasilModel\Action\WaitAction;
+use webignition\BasilModel\Value\EnvironmentValue;
+use webignition\BasilModel\Value\Value;
+use webignition\BasilModel\Value\ValueInterface;
+use webignition\BasilModel\Value\ValueTypes;
 
 class WaitActionTest extends \PHPUnit\Framework\TestCase
 {
-    public function testCreate()
-    {
-        $duration = '10';
-
-        $action = new WaitAction('wait 10', $duration);
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(
+        string $actionString,
+        ValueInterface $duration,
+        string $expectedArguments
+    ) {
+        $action = new WaitAction($actionString, $duration);
 
         $this->assertSame(ActionTypes::WAIT, $action->getType());
-        $this->assertSame('10', $action->getArguments());
+        $this->assertSame($expectedArguments, $action->getArguments());
         $this->assertSame($duration, $action->getDuration());
         $this->assertTrue($action->isRecognised());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'string value' => [
+                'actionString' => 'wait 10',
+                'duration' => new Value(
+                    ValueTypes::STRING,
+                    '10'
+                ),
+                'expectedArguments' => '10',
+            ],
+            'environment parameter value' => [
+                'actionString' => 'wait $env.DURATION',
+                'duration' => new EnvironmentValue(
+                    '$env.DURATION',
+                    'DURATION'
+                ),
+                'expectedArguments' => '$env.DURATION',
+            ],
+        ];
     }
 }
