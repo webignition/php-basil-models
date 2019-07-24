c09f9127e0b4ce9e7bd025631d2e0e4997dc3476
Jon Cram <jon@webignition.net>
2019-07-24 15:06:08 +0100

    Add LiteralValue::createStringValue(), ::createCssSelectorValue(), ::createXpathExpressionValue()

--- src/Value/LiteralValue.php
+++ src/Value/LiteralValue.php
@@ -6,11 +6,26 @@ class LiteralValue extends AbstractValue implements LiteralValueInterface
 {
     private $value;
 
-    public function __construct(string $value)
+    private function __construct(string $type, string $value)
     {
-        parent::__construct(ValueTypes::STRING);
+        parent::__construct($type);
 
         $this->value = $value;
+    }
+
+    public static function createStringValue(string $value)
+    {
+        return new LiteralValue(ValueTypes::STRING, $value);
+    }
+
+    public static function createCssSelectorValue(string $value)
+    {
+        return new LiteralValue(ValueTypes::CSS_SELECTOR, $value);
+    }
+
+    public static function createXpathExpressionValue(string $value)
+    {
+        return new LiteralValue(ValueTypes::XPATH_EXPRESSION, $value);
     }
 
     public function getValue(): string
