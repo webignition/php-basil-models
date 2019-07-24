ff2e07c604686e7d338aac5ce8fe1a4851ec944a
Jon Cram <jon@webignition.net>
2019-07-24 10:32:08 +0100

    Fix IdentifierInterface::getName()

--- src/Identifier/ElementIdentifierInterface.php
+++ src/Identifier/ElementIdentifierInterface.php
@@ -6,7 +6,6 @@ interface ElementIdentifierInterface extends IdentifierInterface
 {
     public function getValue(): string;
     public function getPosition(): int;
-    public function getName(): ?string;
     public function getParentIdentifier(): ?ElementIdentifierInterface;
     public function withParentIdentifier(ElementIdentifierInterface $identifier): ElementIdentifierInterface;
 }
--- src/Identifier/IdentifierInterface.php
+++ src/Identifier/IdentifierInterface.php
@@ -5,6 +5,7 @@ namespace webignition\BasilModel\Identifier;
 interface IdentifierInterface
 {
     public function getType(): string;
+    public function getName(): ?string;
     public function withName(string $name): IdentifierInterface;
     public function __toString(): string;
 }
