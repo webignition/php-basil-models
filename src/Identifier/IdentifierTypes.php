e24a2b2b5b521910724ee7d16a6d39a8cf80914a
Jon Cram <webignition@gmail.com>
2019-07-24 15:49:22 +0100

    Remove css and xpath identifier types (#122)

--- src/Identifier/IdentifierTypes.php
+++ src/Identifier/IdentifierTypes.php
@@ -4,21 +4,15 @@ namespace webignition\BasilModel\Identifier;
 
 class IdentifierTypes
 {
-    const CSS_SELECTOR = 'css-selector';
-    const XPATH_EXPRESSION = 'xpath-expression';
     const PAGE_ELEMENT_REFERENCE = 'page-element-reference';
     const ELEMENT_PARAMETER = 'element-parameter';
     const ELEMENT_SELECTOR = 'element-selector';
 
     const ACTIONABLE_TYPES = [
-        self::CSS_SELECTOR,
-        self::XPATH_EXPRESSION,
         self::ELEMENT_SELECTOR,
     ];
 
     const ASSERTABLE_TYPES = [
-        self::CSS_SELECTOR,
-        self::XPATH_EXPRESSION,
         self::ELEMENT_SELECTOR,
     ];
 }
