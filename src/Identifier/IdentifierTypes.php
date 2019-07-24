bde5ba9e7f69bfce542aef8d61067ec1efd1ecaf
Jon Cram <webignition@gmail.com>
2019-07-24 14:38:53 +0100

    Add IdentifierTypes::ELEMENT_SELECTOR (#113)

--- src/Identifier/IdentifierTypes.php
+++ src/Identifier/IdentifierTypes.php
@@ -8,14 +8,17 @@ class IdentifierTypes
     const XPATH_EXPRESSION = 'xpath-expression';
     const PAGE_ELEMENT_REFERENCE = 'page-element-reference';
     const ELEMENT_PARAMETER = 'element-parameter';
+    const ELEMENT_SELECTOR = 'element-selector';
 
     const ACTIONABLE_TYPES = [
         self::CSS_SELECTOR,
         self::XPATH_EXPRESSION,
+        self::ELEMENT_SELECTOR,
     ];
 
     const ASSERTABLE_TYPES = [
         self::CSS_SELECTOR,
         self::XPATH_EXPRESSION,
+        self::ELEMENT_SELECTOR,
     ];
 }
