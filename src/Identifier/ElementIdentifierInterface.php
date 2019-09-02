a64178d9a416fec7284a358bca7e8a5f3d76bb7c
Jon Cram <jon@webignition.net>
2019-09-02 12:02:04 +0100

    Add ElementIdentifierInterface::withPosition()

--- src/Identifier/ElementIdentifierInterface.php
+++ src/Identifier/ElementIdentifierInterface.php
@@ -5,6 +5,7 @@ namespace webignition\BasilModel\Identifier;
 interface ElementIdentifierInterface extends IdentifierInterface
 {
     public function getPosition(): ?int;
+    public function withPosition(int $position): ElementIdentifierInterface;
     public function getParentIdentifier(): ?ElementIdentifierInterface;
     public function withParentIdentifier(ElementIdentifierInterface $identifier): ElementIdentifierInterface;
 }
