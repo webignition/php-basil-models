13e29656453172472c23b48bb49cf1bad6009969
Jon Cram <webignition@gmail.com>
2019-09-12 12:41:15 +0100

    Annotate exceptions on assertion value interfaces (#225)

--- src/Value/AssertionExaminedValueInterface.php
+++ src/Value/AssertionExaminedValueInterface.php
@@ -2,10 +2,14 @@
 
 namespace webignition\BasilModel\Value;
 
+use webignition\BasilModel\Exception\InvalidAssertionExaminedValueException;
+
 interface AssertionExaminedValueInterface extends ValueInterface, WrappedValueInterface
 {
     /**
      * @return AttributeValueInterface|BrowserProperty|DataParameter|ElementValueInterface|EnvironmentValueInterface|PageProperty|PageElementReference|ReferenceValueInterface
+     *
+     * @throws InvalidAssertionExaminedValueException
      */
     public function getExaminedValue();
 }
